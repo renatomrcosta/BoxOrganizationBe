cba0cd27ba26dce7ce4275a277d32471b150aa96
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-06 21:35:01 +0100

    schema adjustment

--- src/main/resources/db/migration/V20201130152932__create_schema.sql
+++ src/main/resources/db/migration/V20201130152932__create_schema.sql
@@ -1,32 +1,30 @@
 create table "user"
 (
-	id UUID
-		constraint user_pk
-			primary key,
-	firebase_uid TEXT,
-	name TEXT,
-	email TEXT
+    id           UUID
+        constraint user_pk
+            primary key,
+    firebase_uid TEXT,
+    name         TEXT,
+    email        TEXT
 );
 
 create unique index user_firebase_uid_uindex
-	on "user" (firebase_uid);
-
-create type ItemType as enum ('Box', 'Object');
+    on "user" (firebase_uid);
 
 create table item
 (
-	id uuid
-		constraint item_pk
-			primary key,
-    name TEXT,
-    type ItemType NOT NULL,
-	user_id UUID NOT NULL
+    id                uuid
+        constraint item_pk
+            primary key,
+    name              TEXT,
+    type              TEXT NOT NULL, -- Box or Object
+    user_id           UUID NOT NULL
         constraint item_user_id_fk
-            references "user"(id),
-	qr_code text NULL,
-	container_item_id uuid null
+            references "user" (id),
+    qr_code           text NULL,
+    container_item_id uuid null
         constraint item_container_id_fk
-            references item(id)
+            references item (id)
 );
 
 
@@ -35,16 +33,16 @@ create index item_name_index on item (name);
 
 create table image
 (
-	id uuid
-		constraint image_pk
-			primary key,
-	url TEXT
+    id  uuid
+        constraint image_pk
+            primary key,
+    url TEXT
 );
 
 
 create table item_image
 (
-    item_id uuid
+    item_id  uuid
         constraint item_image_item_id_fk
             references item (id),
     image_id uuid
@@ -52,14 +50,17 @@ create table item_image
             references image (id)
 );
 
-create index item_image_item_id_index on item_image(item_id);
+create index item_image_item_id_index on item_image (item_id);
 
 create table item_tag
 (
+    id      uuid
+        constraint item_tag_pk
+            primary key,
     item_id uuid
         constraint item_tag_item_id_fk
             references item (id),
-    tag text
+    tag     text
 );
 
 create index item_tag_item_id_index
