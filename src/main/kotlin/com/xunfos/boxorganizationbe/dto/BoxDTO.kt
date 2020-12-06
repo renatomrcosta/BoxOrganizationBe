61c19e294d84a21a8c9c2e20ef30e7b5a0aa0221
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-06 21:50:05 +0100

    adjusted dtos

--- src/main/kotlin/com/xunfos/boxorganizationbe/dto/BoxDTO.kt
+++ src/main/kotlin/com/xunfos/boxorganizationbe/dto/BoxDTO.kt
@@ -6,5 +6,5 @@ data class BoxDTO(
     val id: UUID? = null,
     val name: String,
     val qrCode: String?,
-    val objects: List<ObjectDTO>,
+    val objects: List<ObjectDTO> = emptyList(),
 )
--- src/main/kotlin/com/xunfos/boxorganizationbe/dto/ObjectDTO.kt
+++ src/main/kotlin/com/xunfos/boxorganizationbe/dto/ObjectDTO.kt
@@ -5,5 +5,5 @@ import java.util.UUID
 data class ObjectDTO(
     val id: UUID? = null,
     val name: String,
-    val tags: List<TagDTO>,
+    val tags: List<TagDTO> = emptyList(),
 )
