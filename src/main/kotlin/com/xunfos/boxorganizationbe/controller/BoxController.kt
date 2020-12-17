72a9d53bdf351664e52e3907dafa4134004430b8
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-17 09:40:14 +0100

    fixed userId for prototype application version

--- src/main/kotlin/com/xunfos/boxorganizationbe/controller/BoxController.kt
+++ src/main/kotlin/com/xunfos/boxorganizationbe/controller/BoxController.kt
@@ -3,6 +3,7 @@ package com.xunfos.boxorganizationbe.controller
 import com.xunfos.boxorganizationbe.dto.BoxDTO
 import com.xunfos.boxorganizationbe.service.BoxService
 import kotlinx.coroutines.flow.Flow
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,32 +15,26 @@ import java.util.UUID
 @RestController
 class BoxController(
     private val boxService: BoxService,
+    @Value("\${user.id}")
+    private val userId: UUID,
 ) {
     @GetMapping("/box")
     suspend fun list(): Flow<BoxDTO> {
-        // TODO - Authorize by userId
-        val userId = UUID.fromString("2fbd2774-06ec-4de7-b657-9617bd9971c5")
         return boxService.getAllBoxesByUserId(userId = userId)
     }
 
     @PostMapping("/box")
     suspend fun add(@RequestBody dto: BoxDTO): BoxDTO {
-        // TODO - Authorize by userId
-        val userId = UUID.fromString("2fbd2774-06ec-4de7-b657-9617bd9971c5")
         return boxService.addBox(userId, dto)
     }
 
     @PutMapping("/box")
     suspend fun update(@RequestBody dto: BoxDTO): BoxDTO {
-        // TODO - Authorize by userId
-        val userId = UUID.fromString("2fbd2774-06ec-4de7-b657-9617bd9971c5")
         return boxService.updateBox(userId, dto)
     }
 
     @DeleteMapping("/box")
     suspend fun delete(@RequestBody dto: BoxDTO): Unit {
-        // TODO - Authorize by userId
-        val userId = UUID.fromString("2fbd2774-06ec-4de7-b657-9617bd9971c5")
         return boxService.removeBox(dto)
     }
 }
