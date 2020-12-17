e7223918381de7a27c2909a414dbe6863b1aa92f
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-17 10:05:15 +0100

    stupid basic auth implementation

---
+++ src/main/kotlin/com/xunfos/boxorganizationbe/configuration/SecurityConfiguration.kt
@@ -0,0 +1,19 @@
+package com.xunfos.boxorganizationbe.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@EnableWebFluxSecurity
+class SecurityConfiguration {
+    @Bean
+    fun securityWebFilterChain(
+        http: ServerHttpSecurity,
+    ): SecurityWebFilterChain =
+        http.authorizeExchange()
+            .pathMatchers("/actuator/*").permitAll()
+            .anyExchange().authenticated()
+            .and().build()
+}
